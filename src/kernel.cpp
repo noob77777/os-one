0523d35a6ea09576e2a8a2cde857b903ed1fc51d
noob77777 <abhishekpal7@outlook.com>
2020-10-27 21:25:28 +0530

    Added: include/types.h

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,14 +1,15 @@
+#include "include/types.h"
 
 void kprintf(const char *str)
 {
-    char *video_memory = (char *)0xb8000;
+    uint8_t *video_memory = (uint8_t *)0xb8000;
     for (int i = 0; str[i] != '\0'; i++)
     {
         video_memory[2 * i] = str[i];
     }
 }
 
-extern "C" void kernel_main(void *multiboot_struct, unsigned int magic_number)
+extern "C" void kernel_main(void *multiboot_struct, uint32_t magic_number)
 {
     kprintf("OS-ONE (version 0.0.1-target=i386)");
     for (;;)
