7521952fc62bcb3942a21b6533242ab271291b1c
noob77777 <abhishekpal7@outlook.com>
2020-11-03 03:06:22 +0530

    Fixed: types.h

--- src/include/types.h
+++ src/include/types.h
@@ -1,7 +1,7 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-typedef char uint8_t;
+typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
