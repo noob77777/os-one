749ddd67f736469af34cd632744c585969eb62b6
noob77777 <abhishekpal7@outlook.com>
2021-02-01 18:42:42 +0530

    Added: Port32bit

--- include/hardware/port.h
+++ include/hardware/port.h
@@ -9,12 +9,13 @@ protected:
     uint16_t port;
     Port(uint16_t port);
 
-public:
     static inline uint8_t read8(uint16_t _port);
     static inline void write8(uint16_t _port, uint8_t _data);
     static inline uint16_t read16(uint16_t _port);
     static inline void write16(uint16_t _port, uint16_t _data);
     static inline void write8_pic(uint16_t _port, uint8_t _data);
+    static inline uint32_t read32(uint16_t _port);
+    static inline void write32(uint16_t _port, uint32_t _data);
 };
 
 class Port8bit : public Port
@@ -41,6 +42,15 @@ public:
 
     uint16_t read();
     void write(uint16_t data);
+};
+
+class Port32bit : public Port
+{
+public:
+    Port32bit(uint16_t port);
+
+    virtual uint32_t read();
+    virtual void write(uint32_t data);
 };
 
 #endif
